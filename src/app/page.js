bde0ce4640ba2a81daea05f606d488a37bdb4dcc
Darwin Koirala <darwinkoirala123@gmail.com>
2025-09-08 00:13:28 +0545

    fix: fixed elastic scrolling issue from touchpad

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,17 @@
 import CompanyPage from "./companies/page";
 import HeroPage from "./hero/page";
 
-    
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll">
-
-      <section className="snap-start h-screen w-screen  bg-gray-700 ">
-        <HeroPage/>
+    <main className="snap-y snap-mandatory h-screen w-screen overflow-y-scroll overscroll-contain">
+      <section className="snap-start h-screen w-screen bg-gray-700">
+        <HeroPage />
       </section>
 
       <section className="snap-start h-screen w-screen bg-gray-700">
-        <CompanyPage/>
+        <CompanyPage />
       </section>
-
     </main>
   );
 }
+
